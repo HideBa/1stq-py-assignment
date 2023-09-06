a5fef8b513fb57f691f601987c466d6b1e87b5fa
HideBa <baba.papa1120.ba@gmail.com>
2023-09-06 12:28:37 +0200

    fix easy mistake for assignment1

--- assignment1/tienstra.py
+++ assignment1/tienstra.py
@@ -25,21 +25,27 @@ def k(angle_corner, angle_interior):
 
 
 # alpha, beta, gamma are in degree
+# this will return (Px, Py)
 def tienstra(ax, ay, bx, by, cx, cy, alpha, beta):
     node_ab = distance(ax, ay, bx, by)
     node_bc = distance(bx, by, cx, cy)
     node_ac = distance(ax, ay, cx, cy)
-    gamma = 360 - alpha - beta
 
-    k1 = k(angle(node_ab, node_ac, node_bc), math.radians(alpha))
-    k2 = k(angle(node_ab, node_bc, node_ac), math.radians(beta))
-    k3 = k(angle(node_ac, node_bc, node_ab), math.radians(gamma))
+    gamma = 360 - alpha - beta  # degree
 
-    px = (k1 * ax + k2 * bx + k3 + cx) / (k1 + k2 + k3)
-    py = (k1 * ay + k2 * by + k3 + cy) / (k1 + k2 + k3)
+    angle_a = angle(node_ab, node_ac, node_bc)
+    angle_b = angle(node_ab, node_bc, node_ac)
+    angle_c = angle(node_ac, node_bc, node_ab)
+
+    k1 = k(angle_a, math.radians(alpha))
+    k2 = k(angle_b, math.radians(beta))
+    k3 = k(angle_c, math.radians(gamma))
+
+    px = (k1 * ax + k2 * bx + k3 * cx) / (k1 + k2 + k3)
+    py = (k1 * ay + k2 * by + k3 * cy) / (k1 + k2 + k3)
     print("(Px, Py) is " + "(" + str(px) + ", " + str(py) + ")")
     return (px, py)
 
 
-# result is (255058.40780035098, 636068.8075068599)
+# result(Px, Py) is (2082.1882960042926, 5902.852592437437)
 tienstra(1000.0, 5300.0, 2200.0, 6300.0, 3100.0, 5000.0, 115.052, 109.3045)
