f111fffb0fddae15bce06b20884ff7c14a387c7b
HideBa <baba.papa1120.ba@gmail.com>
2023-09-28 16:44:44 +0200

    implemented pattern

--- assignment2/patterns.py
+++ assignment2/patterns.py
@@ -4,9 +4,8 @@
 
 
 def wkt(p1, p2, p3, p4):
-    points = []
+    points = []  # [(p1[0], p1[1]), (p2[0], p2[1]), (p3[0], p3[1]), (p4[0], p4[1])
     for point in [p1, p2, p3, p4]:
-        # TODO: consider when after deciaml point is 0
         points.append(("{:.6f}".format(point[0]), "{:.6f}".format(point[1])))
 
     return "POLYGON (({x1} {y1}, {x2} {y2}, {x3} {y3}, {x4} {y4}, {x1} {y1}))".format(
@@ -22,72 +21,83 @@ def wkt(p1, p2, p3, p4):
 
 
 def pattern_a(level, center, size, ratio, file_nm):
-    if level < 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
-        raise Exception(
-            "level, size, ratio cannot be less than zero. center and file_nm also should be provided"
-        )
+    if level <= 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
+        return
 
     point1 = (center[0] - size, center[1] - size)
     point2 = (center[0] + size, center[1] - size)
     point3 = (center[0] + size, center[1] + size)
     point4 = (center[0] - size, center[1] + size)
-    f = open(file_nm, "a")
-    f.write("{};\n".format(wkt(point1, point2, point3, point4)))
-    f.close()
+
+    with open(file_nm, "a") as f:
+        f.write("{};\n".format(wkt(point1, point2, point3, point4)))
     level = level - 1
-    if level > 0:
-        for point in [point1, point2, point3, point4]:
-            pattern_a(level, point, size / ratio, ratio, file_nm)
+    for point in [point1, point2, point3, point4]:
+        pattern_a(level, point, size / ratio, ratio, file_nm)
 
 
 def pattern_b(level, center, size, ratio, file_nm):
-    if level < 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
-        raise Exception(
-            "level, size, ratio cannot be less than zero. center and file_nm also should be provided"
-        )
+    if level <= 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
+        return
 
     point1 = (center[0] - size, center[1] - size)
     point2 = (center[0] + size, center[1] - size)
     point3 = (center[0] + size, center[1] + size)
     point4 = (center[0] - size, center[1] + size)
     level = level - 1
-    if level > 0:
-        for point in [point1, point2, point3, point4]:
-            pattern_b(level, point, size / ratio, ratio, file_nm)
-    f = open(file_nm, "a")
-    f.write("{};\n".format(wkt(point1, point2, point3, point4)))
-    f.close()
+    for point in [point1, point2, point3, point4]:
+        pattern_b(level, point, size / ratio, ratio, file_nm)
+    with open(file_nm, "a") as f:
+        f.write("{};\n".format(wkt(point1, point2, point3, point4)))
 
 
 def pattern_c(level, center, size, ratio, file_nm):
-    if level < 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
-        raise Exception(
-            "level, size, ratio cannot be less than zero. center and file_nm also should be provided"
-        )
+    if level <= 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
+        return
 
     point1 = (center[0] - size, center[1] - size)
     point2 = (center[0] + size, center[1] - size)
     point3 = (center[0] + size, center[1] + size)
     point4 = (center[0] - size, center[1] + size)
-    points = [point1, point2, point3, point4]
+    point1_2 = [point1, point2]
+    point3_4 = [point3, point4]
+
     level = level - 1
-    # TODO: fix when level is zero
-    if level > 0:
-        for i in range(len(points)):
-            if i == 0 or i == 1:
-                f = open(file_nm, "a")
-                f.write("{};\n".format(wkt(point1, point2, point3, point4)))
-                f.close()
-                pattern_c(level, points[i], size / ratio, ratio, file_nm)
-            else:
-                pattern_c(level, points[i], size / ratio, ratio, file_nm)
-                f = open(file_nm, "a")
-                f.write("{};\n".format(wkt(point1, point2, point3, point4)))
-                f.close()
+
+    for point in point3_4:
+        pattern_c(level, point, size / ratio, ratio, file_nm)
+
+    with open(file_nm, "a") as f:
+        f.write("{};\n".format(wkt(point1, point2, point3, point4)))
+
+    for point in point1_2:
+        pattern_c(level, point, size / ratio, ratio, file_nm)
+
+
+# def pattern_c(level, center, size, ratio, file_nm):
+#     if level <= 0 or size < 0 or ratio < 0 or center is None or file_nm == "":
+#         return
+
+#     point1 = (center[0] - size, center[1] - size)
+#     point2 = (center[0] + size, center[1] - size)
+#     point3 = (center[0] + size, center[1] + size)
+#     point4 = (center[0] - size, center[1] + size)
+#     points = [point1, point2, point3, point4]
+#     level = level - 1
+#     # TODO: fix when level is zero
+#     for i in range(len(points)):
+#         if i == 0 or i == 1:
+#             with open(file_nm, "a") as f:
+#                 f.write("{};\n".format(wkt(point1, point2, point3, point4)))
+#             pattern_c(level, points[i], size / ratio, ratio, file_nm)
+#         else:
+#             pattern_c(level, points[i], size / ratio, ratio, file_nm)
+#             with open(file_nm, "a") as f:
+#                 f.write("{};\n".format(wkt(point1, point2, point3, point4)))
 
 
 def main(
-    n=3, c=(0.0, 0.0), size=10.0, ratio=2.2
+    n=1, c=(0.0, 0.0), size=10.0, ratio=2.2
 ):  # note, optional arguments, see Sec 13.5 of ThinkPython2
     """The starting point of this program.
     Writes for every output file the first line and allows
@@ -103,9 +113,8 @@ def main(
     funcs = [pattern_a, pattern_b, pattern_c]
     file_nms = ["pattern_a.txt", "pattern_b.txt", "pattern_c.txt"]
     for func, file_nm_out in zip(funcs, file_nms):
-        f = open(file_nm_out, "w")
-        f.write("level;geometry\n")
-        f.close()
+        with open(file_nm_out, "w") as f:
+            f.write("level;geometry\n")  # first line of the file
         func(level=n, center=c, size=size, ratio=ratio, file_nm=file_nm_out)
 
 
